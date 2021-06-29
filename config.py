d5c25d7ccc0d4172a8531dce0005f6f818cd6f03
Shashwat Singh <singhshashwat1002@gmail.com>
2021-06-29 22:51:14 +0530

    Move all scrapping helpers to scrapping.py and introduce config.py
    Some of the global variables being used will now be accepted as function arguments
    
    basically "data_dir"
    is taken by functions for scrapping purposes.

---
+++ config.py
@@ -0,0 +1,2 @@
+DATA_FILE_NAMES = ["english_corpus.txt", "english_corpus_cleaned.txt", "url_file_english.txt", "visited_urls_english.txt"]
+ENGLISH_URL_SEED = "https://en.wikipedia.org/wiki/The_Godfather"
