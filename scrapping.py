8ff3cbbb3cb1f84d24a57b4894abe73d4cd3f162
Shashwat Singh <singhshashwat1002@gmail.com>
2021-06-29 15:59:49 +0530

    make the scrapping code turn relative urls to complete urls

--- scrapping.py
+++ scrapping.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import urllib.parse
 
 
 def scrap_english_page(url, corpus_file, url_file):
@@ -7,7 +8,7 @@ def scrap_english_page(url, corpus_file, url_file):
     response = requests.get(url)
     soup_for_land_page = BeautifulSoup(response.content, 'html.parser')
     all_paragraphs = soup_for_land_page.find_all('p')
-
+    parsed_url = urllib.parse.urlparse(url)
     for para in all_paragraphs:
         for content in para.contents:
             # each content has a string object
@@ -20,6 +21,12 @@ def scrap_english_page(url, corpus_file, url_file):
             if content.name == 'a':
                 # is a url
                 url_string = content['href']
+                current_url_parsed = urllib.parse.urlparse(url_string)
+                if current_url_parsed.netloc == '':
+                    # if netloc is empty then that means the url is relative
+                    # thus we need to make it an absolute url
+                    url_string = "{}://{}/{}".format(parsed_url.scheme, parsed_url.netloc, url_string)
+                    # basically we're adding the relative part after the base domain
                 url_file.write(url_string)
                 url_file.write("\n")
 
