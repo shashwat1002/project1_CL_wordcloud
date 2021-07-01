7d24a9f657d49ddf8d92bc2f5c64685b17361794
Shashwat Singh <singhshashwat1002@gmail.com>
2021-07-01 11:39:06 +0530

    improve formatting and hindi related file

--- config.py
+++ config.py
@@ -1,2 +1,9 @@
-DATA_FILE_NAMES = ["english_corpus.txt", "english_corpus_cleaned.txt", "url_file_english.txt", "visited_urls_english.txt"]
+DATA_FILE_NAMES = ["english_corpus.txt",
+                   "english_corpus_cleaned.txt",
+                   "url_file_english.txt",
+                   "visited_urls_english.txt",
+                   "hindi_corpus.txt",
+                   "visited_urls_hindi.txt",
+                   "url_file_hindi.txt"]
 ENGLISH_URL_SEED = "https://en.wikipedia.org/wiki/The_Godfather"
+HINDI_URL_SEED = "https://hi.wikipedia.org/wiki/%E0%A4%A8%E0%A4%BE%E0%A4%9F%E0%A4%95"
